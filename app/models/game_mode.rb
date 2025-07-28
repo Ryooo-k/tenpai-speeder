fbe139d54a34be4d6fcce5695e6604740948aeaa
ryo <ta090972@icloud.com>
2025-07-28 10:16:06 +0900

    モデルの関連付け・バリデーションを修正

--- app/models/game_mode.rb
+++ app/models/game_mode.rb
@@ -3,8 +3,16 @@
 class GameMode < ApplicationRecord
   has_many :games
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :round_type, presence: true
   validates :mode_type, presence: true
 
+  enum :round_type, {
+    ikkyoku: 0,
+    tonpuu: 1,
+    tonnan: 2
+  }
   enum :mode_type, {
     training: 0,
     match: 1
