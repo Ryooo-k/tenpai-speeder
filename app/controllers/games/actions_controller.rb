1e31b70970a2d167bbc60e889bb1aa5442065f93
ryo <ta090972@icloud.com>
2025-08-21 16:32:26 +0900

    ツモ、打牌選択、打牌のルーティング、コントローラー、ビューを作成

--- app/controllers/games/actions_controller.rb
+++ app/controllers/games/actions_controller.rb
@@ -1,6 +1,65 @@
 # frozen_string_literal: true
 
 class Games::ActionsController < ApplicationController
-  def create
+  before_action :set_game
+
+  def draw
+    current_player.draw
+    auto_draw = false
+    auto_choose = true if current_player.ai?
+    redirect_to game_play_path(@game, auto_draw:, auto_choose:)
   end
+
+  # ai用打牌選択アクション
+  def choose
+    chosen_tile_id = current_player.choose
+    auto_draw = false
+    auto_discard = true
+    redirect_to game_play_path(@game, auto_draw:, auto_discard:, chosen_tile_id:)
+  end
+
+  def discard
+    current_player.discard(action_params[:selected_tile_id])
+    auto_draw = true
+    @game.advance
+    redirect_to game_play_path(@game, auto_draw:)
+  end
+
+  private
+
+    def set_game
+      @game = Game.includes(
+        :game_mode,
+        { tiles: :base_tile },
+        { players: [
+          :user,
+          :ai,
+          { game_records: :honba }
+        ] },
+        { rounds: [
+          honbas: [
+            { tile_orders: { tile: :base_tile } },
+            { turns: {
+              steps: [
+                { actions: :player },
+                { player_states: [
+                  { hands: { tile: :base_tile } },
+                  { melds:  [ { tile: :base_tile }, :action ] },
+                  { rivers: { tile: :base_tile } },
+                  :player
+                ] }
+              ]
+            } }
+          ]
+        ] }
+      ).find(params[:game_id])
+    end
+
+    def current_player
+      @game.current_player
+    end
+
+    def action_params
+      params.expect(:selected_tile_id)
+    end
 end
--- app/controllers/games/plays_controller.rb
+++ app/controllers/games/plays_controller.rb
@@ -1,33 +1,52 @@
 # frozen_string_literal: true
 
 class Games::PlaysController < ApplicationController
-  def show
-    @game = Game.includes(
-      :game_mode,
-      { tiles: :base_tile },
-      { players: [
-        :user,
-        :ai,
-        { game_records: :honba }
-      ] },
-      { rounds: [
-        honbas: [
-          { tile_orders: { tile: :base_tile } },
-          { turns: {
-            steps: [
-              { actions: :player },
-              { player_states: [
-                { hands: { tile: :base_tile } },
-                { melds:  [ { tile: :base_tile }, :action ] },
-                { rivers: { tile: :base_tile } },
-                :player
-              ] }
-            ]
-          } }
-        ]
-      ] }
-    ).find(params[:game_id])
+  before_action :set_game
+  before_action :set_players, only: :show
 
-    @viewer = @game.players.user
+  def show
+    @auto_draw = play_params[:auto_draw]
+    @auto_choose = play_params[:auto_choose]
+    @auto_discard = play_params[:auto_discard]
+    @chosen_tile_id = play_params[:chosen_tile_id]
   end
+
+  private
+
+    def set_game
+      @game = Game.includes(
+        :game_mode,
+        { tiles: :base_tile },
+        { players: [
+          :user,
+          :ai,
+          { game_records: :honba }
+        ] },
+        { rounds: [
+          honbas: [
+            { tile_orders: { tile: :base_tile } },
+            { turns: {
+              steps: [
+                { actions: :player },
+                { player_states: [
+                  { hands: { tile: :base_tile } },
+                  { melds:  [ { tile: :base_tile }, :action ] },
+                  { rivers: { tile: :base_tile } },
+                  :player
+                ] }
+              ]
+            } }
+          ]
+        ] }
+      ).find(params[:game_id])
+    end
+
+    def set_players
+      @user_player = @game.user_player
+      @opponents = @game.opponents
+    end
+
+    def play_params
+      params.expect([:auto_draw, :auto_choose, :chosen_tile_id])
+    end
 end
