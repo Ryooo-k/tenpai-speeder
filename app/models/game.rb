1d613a1f17de0893fad75aece8781679c0798a2b
ryo <ta090972@icloud.com>
2025-08-24 16:40:21 +0900

    Gameモデルにメソッドを追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,7 @@ class Game < ApplicationRecord
   TILES_PER_KIND = 4
   AKA_DORA_TILE_CODES = [ 4, 13, 22 ] # 5萬、5筒、5索の牌コード
   INITIAL_HAND_SIZE = 13
+  PLAYERS_COUNT = 4
 
   belongs_to :game_mode
 
@@ -34,14 +35,40 @@ class Game < ApplicationRecord
   def deal_initial_hands
     players.ordered.each do |player|
       INITIAL_HAND_SIZE.times do |_|
-        tile = current_honba.top_tile
-        player.receive(tile)
+        player.receive(top_tile)
+        increase_draw_count
       end
     end
   end
 
-  def current_honba
-    rounds.last.honbas.last
+  def user_player
+    players.where.not(user_id: nil).first
+  end
+
+  def opponents
+    players.where.not(ai_id: nil)
+  end
+
+  def current_player
+    players.find_by!(seat_order: current_seat_number)
+  end
+
+  def advance_current_player!
+    next_seat_number = (current_seat_number + 1) % PLAYERS_COUNT
+    update!(current_seat_number: next_seat_number)
+  end
+
+  def draw_count
+    current_honba.draw_count
+  end
+
+  def draw_for_current_player
+    current_player.draw(top_tile, next_step)
+    increase_draw_count
+  end
+
+  def discard_for_current_player(tile_id)
+    current_player.discard(tile_id, next_step)
   end
 
   private
@@ -51,23 +78,56 @@ class Game < ApplicationRecord
       rounds.create!
     end
 
-  def setup_tiles
-    base_tiles = BaseTile.all.index_by(&:code)
+    def setup_tiles
+      base_tiles = BaseTile.all.index_by(&:code)
 
-    base_tiles.keys.each do |code|
-      base_tile = base_tiles[code]
-      TILES_PER_KIND.times do |kind|
-        is_aka_dora = aka_dora_tile?(code, kind)
-        tiles.create!(kind:, aka: is_aka_dora, base_tile:)
+      base_tiles.keys.each do |code|
+        base_tile = base_tiles[code]
+        TILES_PER_KIND.times do |kind|
+          is_aka_dora = aka_dora_tile?(code, kind)
+          tiles.create!(kind:, aka: is_aka_dora, base_tile:)
+        end
       end
     end
-  end
 
-  def aka_dora_tile?(code, kind)
-    game_mode.aka_dora? && AKA_DORA_TILE_CODES.include?(code) && kind.zero?
-  end
+    def aka_dora_tile?(code, kind)
+      game_mode.aka_dora? && AKA_DORA_TILE_CODES.include?(code) && kind.zero?
+    end
 
-  def current_step
-    rounds.last.honbas.last.turns.last.steps.last
-  end
+    def current_round
+      rounds.last
+    end
+
+    def current_honba
+      current_round.honbas.last
+    end
+
+    def current_turn
+      current_honba.turns.last
+    end
+
+    def current_step
+      current_turn.steps.last
+    end
+
+    def next_step_number
+      current_step.number + 1
+    end
+
+    def top_tile
+      current_honba.top_tile
+    end
+
+    def host
+      players.find_by(seat_order: current_round.host_position)
+    end
+
+    def increase_draw_count
+      current_honba.increment!(:draw_count)
+    end
+
+    def next_step
+      next_step_number = current_step.number + 1
+      current_turn.create_next_step(next_step_number)
+    end
 end
