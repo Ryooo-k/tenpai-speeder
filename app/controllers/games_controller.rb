d379b97b0541df28e11f311e3984f4686331f340
ryo <ta090972@icloud.com>
2025-08-24 14:51:46 +0900

    試合開始時にauto_drawフラグを渡す

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,16 +5,12 @@ class GamesController < ApplicationController
     game_mode_id = params.expect(:game_mode_id)
     game_mode = GameMode.find(game_mode_id)
     game = Game.new(game_mode:)
-    ai_version = '1.0'
-
-    # ユーザー認証は別途、実装する。
-    current_user = User.find_by(name: :guest)
-    ai = Ai.find_by(version: ai_version)
+    ai = Ai.find_by!(version: '1.0')
 
     if game.save
       game.setup_players(current_user, ai)
       game.deal_initial_hands
-      redirect_to game_play_path(game)
+      redirect_to game_play_path(game, auto_draw: true)
     else
       render :home
     end
