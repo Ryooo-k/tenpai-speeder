fc05bbdd98f15264e85a5ccca3ce4c6adfceb68f
ryo <ta090972@icloud.com>
2025-08-30 14:53:20 +0900

    河を表示されるようにした

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -10,4 +10,14 @@ module GamesHelper
       'absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 origin-center rotate-90'
     end
   end
+
+  def build_river_position_class(player, user)
+    if player.shimocha?(user)
+      'absolute top-1/2 right-1/20 -translate-x-1/2 -translate-y-1/2 -rotate-90'
+    elsif player.toimen?(user)
+      'absolute top-1/20 left-1/2 -translate-x-1/2 rotate-180'
+    else
+      'absolute top-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90'
+    end
+  end
 end
