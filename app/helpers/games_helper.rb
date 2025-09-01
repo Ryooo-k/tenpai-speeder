3df51392a3c7afe62b7462be7e746a1d28a12fcc
ryo <ta090972@icloud.com>
2025-09-01 14:36:46 +0900

    プレイヤーの名前、自風、スコアを表示 - relation_from_userメソッドでuserからみてどの位置かを判定 - relation_from_userメソッドの返り値を元にclassを識別 - GamesHelper内のメソッドは、relation_from_userメソッドに統一し、関連ファイルを修正

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,23 +1,40 @@
 # frozen_string_literal: true
 
 module GamesHelper
-  def build_hand_position_class(player, user)
-    if player.shimocha?(user)
-      'absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 origin-center -rotate-90'
-    elsif player.toimen?(user)
-      'absolute left-1/2 -translate-x-1/2 rotate-180'
-    else
-      'absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 origin-center rotate-90'
+  def build_hand_position_class(player)
+    case player.relation_from_user
+    when :shimocha
+      'right-0 top-1/2 translate-x-1/2 -translate-y-1/2 origin-center -rotate-90'
+    when :toimen
+      'left-1/2 -translate-x-1/2 rotate-180'
+    when :kamicha
+      'left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 origin-center rotate-90'
+    when :self
+      'left-1/2 bottom-0 -translate-x-1/2'
     end
   end
 
-  def build_river_position_class(player, user)
-    if player.shimocha?(user)
-      'absolute top-1/2 right-1/20 -translate-x-1/2 -translate-y-1/2 -rotate-90'
-    elsif player.toimen?(user)
-      'absolute top-1/20 left-1/2 -translate-x-1/2 rotate-180'
-    else
-      'absolute top-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90'
+  def build_river_position_class(player)
+    case player.relation_from_user
+    when :shimocha
+      'top-1/2 right-1/20 -translate-x-1/2 -translate-y-1/2 -rotate-90'
+    when :toimen
+      'top-1/20 left-1/2 -translate-x-1/2 rotate-180'
+    when :kamicha
+      'top-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90'
+    end
+  end
+
+  def build_player_info_class(player)
+    case player.relation_from_user
+    when :shimocha
+      'right-0 top-1/2 -translate-y-1/2 -rotate-90'
+    when :toimen
+      'left-1/2 -translate-x-1/2 rotate-180'
+    when :kamicha
+      'left-0 top-1/2 -translate-y-1/2 rotate-90'
+    when :self
+      'left-1/2 bottom-0 -translate-x-1/2'
     end
   end
 end
