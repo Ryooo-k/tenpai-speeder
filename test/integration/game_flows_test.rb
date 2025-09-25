ac5d140a762fe20450f5536f5dc8d7d66acba7d3
ryo <ta090972@icloud.com>
2025-09-26 05:14:38 +0900

    helper、mockをテスト全体で使用可能にした

--- test/integration/game_flows_test.rb
+++ test/integration/game_flows_test.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'helpers/game_test_helper'
-require 'minitest/mock'
 
 class GameFlowsTest < ActionDispatch::IntegrationTest
   include GameTestHelper
