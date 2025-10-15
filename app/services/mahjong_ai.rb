647c371bcab0ce6de1b0f0d44373ebb052d5bd2d
ryo <ta090972@icloud.com>
2025-10-15 15:42:01 +0900

    q_netをキャッシュして再利用するようにした

--- app/services/mahjong_ai.rb
+++ app/services/mahjong_ai.rb
@@ -7,11 +7,7 @@ module MahjongAi
     def infer(game, player)
       raise UnsupportedAi, 'AIバージョンが未対応です' unless supported_ai_version?(player)
 
-      config = AI_CONFIGS[player.ai_version]
-      q_net = QNet.new(config['input_size'], config['hidden_layers'], config['output_size'])
-      q_net.load_state_dict(Torch.load(config['path']))
-      q_net.eval
-
+      q_net = QNetLoader.fetch(player)
       states = StateEncoder.call(game, player)
       output = q_net.call(states)
       output.argmax.item
