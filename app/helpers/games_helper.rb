80024d1eb63e1aedf824f1f28ae2abc9f58dcfc7
ryo <ta090972@icloud.com>
2025-10-22 17:16:47 +0900

    ヘルパーメソッドを修正 - 使われていないメソッドを削除 - ディレクトリ変更に伴いpathを修正

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -27,26 +27,13 @@ module GamesHelper
     end
   end
 
-  def build_player_info_class(player)
-    case player.relation_from_user
-    when :shimocha
-      'right-0 top-1/2 -translate-y-1/2 -rotate-90'
-    when :toimen
-      'left-1/2 -translate-x-1/2 rotate-180'
-    when :kamicha
-      'left-0 top-1/2 -translate-y-1/2 rotate-90'
-    when :self
-      'left-1/2 bottom-0 -translate-x-1/2'
-    end
-  end
-
   def build_hand_partial_path(event, game, player)
-    return 'games/players/hand_plain' if game.current_player.ai? || player.ai?
+    return 'games/mahjong_table/player/hand_plain' if game.current_player.ai? || player.ai?
 
     case event.to_sym
-    when :choose        then 'games/players/hand_form'
-    when :riichi_choose then 'games/players/riichi_form'
-    else                     'games/players/hand_plain'
+    when :choose        then 'games/mahjong_table/player/hand_form'
+    when :riichi_choose then 'games/mahjong_table/player/riichi_form'
+    else                     'games/mahjong_table/player/hand_plain'
     end
   end
 
