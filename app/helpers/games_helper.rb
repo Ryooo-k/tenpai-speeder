5a5ca1ccf474cbfdc8c51fb4faad63c2a7be5c71
ryo <ta090972@icloud.com>
2025-10-28 00:02:02 +0900

    河の位置を調整

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -17,13 +17,13 @@ module GamesHelper
   def build_river_position_class(player)
     case player.relation_from_user
     when :shimocha
-      'top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2 -rotate-90'
+      'top-[62.8%] left-2/3 -rotate-90'
     when :toimen
-      'top-1/9 left-1/2 -translate-x-1/2 rotate-180'
+      'top-1/3 left-[59.6%] rotate-180'
     when :kamicha
-      'top-1/2 left-1/4 -translate-x-1/2 -translate-y-1/2 rotate-90'
+      'top-[37.2%] left-1/3 rotate-90'
     when :self
-      'top-2/3 left-1/2 -translate-x-1/2'
+      'top-2/3 left-[40.4%]'
     end
   end
 
@@ -42,7 +42,7 @@ module GamesHelper
     when :shimocha
       'right-0 top-1/2 translate-x-1/2 -translate-y-1/2 origin-center -rotate-90'
     when :toimen
-      'left-1/2 top-0 -translate-x-1/2 rotate-180'
+      'left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 rotate-180'
     when :kamicha
       'left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 origin-center rotate-90'
     when :self
