7074a9c5edd88704e268d6790c6cc3597219df6f
ryo <ta090972@icloud.com>
2025-10-31 07:29:54 +0900

    バリデーションを修正

--- app/models/game_mode.rb
+++ app/models/game_mode.rb
@@ -5,14 +5,9 @@ class GameMode < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :round_type, presence: true
+  validates :round_count, presence: true
   validates :mode_type, presence: true
 
-  enum :round_type, {
-    ikkyoku: 0,
-    tonpuu: 1,
-    tonnan: 2
-  }
   enum :mode_type, {
     training: 0,
     match: 1
