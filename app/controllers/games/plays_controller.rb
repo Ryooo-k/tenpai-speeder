69074c9e0b80a53ff54909e518a0efc8c4eee08d
ryo <ta090972@icloud.com>
2025-12-14 22:19:26 +0900

    play画面のエラーメッセージが表示されない不具合を修正した

--- app/controllers/games/plays_controller.rb
+++ app/controllers/games/plays_controller.rb
@@ -19,13 +19,19 @@ class Games::PlaysController < ApplicationController
   rescue GameFlow::SaveError => e
     Rails.logger.error("[GameFlow] SaveError: #{e.message} (#{e.cause&.class})")
     respond_to do |format|
-      format.turbo_stream { redirect_to game_play_path(@game), alert: 'ゲームの保存に失敗しました。時間をおいて再度お試しください。' }
+      format.turbo_stream do
+        flash.now[:alert] = 'ゲームの保存に失敗しました。時間をおいて再度お試しください。'
+        render :error
+      end
       format.html { redirect_to game_play_path(@game), alert: 'ゲームの保存に失敗しました。時間をおいて再度お試しください。' }
     end
   rescue GameFlow::UnknownEvent => e
     Rails.logger.warn("[GameFlow] UnknownEvent: #{e.message} (game_id=#{@game.id}, event=#{params[:event]})")
     respond_to do |format|
-      format.turbo_stream { redirect_to game_play_path(@game), alert: e.message }
+      format.turbo_stream do
+        flash.now[:alert] = e.message
+        render :error
+      end
       format.html { redirect_to game_play_path(@game), alert: e.message }
     end
   end
@@ -43,7 +49,10 @@ class Games::PlaysController < ApplicationController
   rescue ActiveRecord::ActiveRecordError => e
     Rails.logger.error("[GameFlow] UndoError: #{e.message} (#{e.class})")
     respond_to do |format|
-      format.turbo_stream { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
+      format.turbo_stream do
+        flash.now[:alert] = 'ゲームの復元に失敗しました。時間をおいて再度お試しください。'
+        render :error
+      end
       format.html { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
     end
   end
@@ -61,7 +70,10 @@ class Games::PlaysController < ApplicationController
   rescue ActiveRecord::ActiveRecordError => e
     Rails.logger.error("[GameFlow] RedoError: #{e.message} (#{e.class})")
     respond_to do |format|
-      format.turbo_stream { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
+      format.turbo_stream do
+        flash.now[:alert] = 'ゲームの復元に失敗しました。時間をおいて再度お試しください。'
+        render :error
+      end
       format.html { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
     end
   end
@@ -79,7 +91,10 @@ class Games::PlaysController < ApplicationController
   rescue ActiveRecord::ActiveRecordError => e
     Rails.logger.error("[GameFlow] PlayBackError: #{e.message} (#{e.class})")
     respond_to do |format|
-      format.turbo_stream { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
+      format.turbo_stream do
+        flash.now[:alert] = 'ゲームの復元に失敗しました。時間をおいて再度お試しください。'
+        render :error
+      end
       format.html { redirect_to game_play_path(@game), alert: 'ゲームの復元に失敗しました。時間をおいて再度お試しください。' }
     end
   end
